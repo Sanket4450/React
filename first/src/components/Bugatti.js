231b4a78b92ed363dadc78c438732832231ef23b
Sanket4450 <www.talaviyasanket50@gmail.com>
2024-01-18 09:48:29 +0530

    added car price & color

--- first/src/components/Bugatti.js
+++ first/src/components/Bugatti.js
@@ -2,9 +2,14 @@ import { cars } from '../data/cars'
 
 import React from 'react'
 
-function Bugatti({ imgURL }) {
+function Bugatti({ imgURL, color, price = 23 }) {
     return (
-        <img src={imgURL} alt='Bugatti' style={{ width: '20vw', borderRadius: 2 }} />
+        <div>
+            <img src={imgURL} alt='Bugatti' style={{ width: '20vw', height: '14vw', borderRadius: 2 }} />
+            <h2 style={{textAlign: 'center', fontSize: '2.5vw', fontFamily: 'sans-serif'}}>{color}</h2>
+            <h2 style={{textAlign: 'center', fontSize: '2vw', fontFamily: 'monospace'}}>{price}Cr</h2>
+            <p style={{textAlign: 'center', fontSize: '1.5vw'}}>{price >= 35 && '(costly)'}</p>
+        </div>
     )
 }
 
@@ -12,7 +17,7 @@ export default function BugattiCars() {
     return (
         <div style={{ display: 'flex', justifyContent: 'space-around', backgroundColor: '#f5f5f5', padding: '40px 0' }}>
             {
-                cars.map(car => <Bugatti imgURL={car.image} />)
+                cars.map(car => <Bugatti imgURL={car.image} color={car.color} price={car.price} />)
             }
         </div>
     )
--- first/src/data/cars.js
+++ first/src/data/cars.js
@@ -1,18 +1,26 @@
 export const cars = [
     {
         id: 1,
-        image: "https://newsroomcdn.bugatti.com/w_1800,q_75/s3-newsroom-bugatti/7712a21b-5656-452d-aa2a-37cf09396622.webp"
+        image: "https://newsroomcdn.bugatti.com/w_1800,q_75/s3-newsroom-bugatti/7712a21b-5656-452d-aa2a-37cf09396622.webp",
+        color: "Blue",
+        price: 41
     },
     {
         id: 1,
-        image: "https://www.hardwarezone.com.sg/thumbs/544049/og.jpg"
+        image: "https://www.hardwarezone.com.sg/thumbs/544049/og.jpg",
+        color: "Gray",
+        price: 32
     },
     {
         id: 1,
-        image: "https://www.alainclass.com/wp-content/uploads/2020/09/sdh.jpg"
+        image: "https://www.alainclass.com/wp-content/uploads/2020/09/sdh.jpg",
+        color: "Black",
+        price: 36
     },
     {
         id: 1,
-        image: "https://www.carandbike.com/_next/image?url=https%3A%2F%2Fc.ndtvimg.com%2F2022-04%2Fgm76419_bugatti-chiron-super-sport_625x300_05_April_22.jpg&w=750&q=75"
+        image: "https://www.carandbike.com/_next/image?url=https%3A%2F%2Fc.ndtvimg.com%2F2022-04%2Fgm76419_bugatti-chiron-super-sport_625x300_05_April_22.jpg&w=750&q=75",
+        color: "Silver",
+        price: 27
     }
 ]
