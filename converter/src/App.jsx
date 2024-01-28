a7340ce9fa52dd584f9df8a081cb1768af2bdb7b
Sanket4450 <www.talaviyasanket50@gmail.com>
2024-01-28 23:26:51 +0530

    added tailwind config

--- converter/src/App.jsx
+++ converter/src/App.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react'
+
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-
+      <h1 className=' bg-red-500 text-white text-2xl font-sans text-center py-2'>Sanket Talaviya</h1>
     </>
   )
 }
