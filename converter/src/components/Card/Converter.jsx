d3745470ca4e248da0f9ed99b21e9fb9b241345f
Sanket4450 <www.talaviyasanket50@gmail.com>
2024-01-30 00:53:10 +0530

    added switch option functionality

--- converter/src/components/Card/Converter.jsx
+++ converter/src/components/Card/Converter.jsx
@@ -2,14 +2,14 @@ import { SelectMenu } from '../ui/SelectMenu'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRightLeft } from '@fortawesome/free-solid-svg-icons'
 
-export function Converter({ options, fromOption, toOption, setFromOption, setToOption }) {
+export function Converter({ options, fromOption, toOption, setFromOption, setToOption, switchOptions }) {
     return (
         <div className='flex justify-between items-center text-lg sm:text-xl lg:text-2xl'>
             <div>
                 <p className='py-3'>From</p>
                 <SelectMenu options={options} option={fromOption} selectOption={setFromOption} />
             </div>
-            <FontAwesomeIcon icon={faRightLeft} className='text-2xl sm:text-3xl' />
+            <FontAwesomeIcon icon={faRightLeft} onClick={switchOptions} className='text-2xl sm:text-3xl hover:cursor-pointer' />
             <div>
                 <p className='py-3'>To</p>
                 <SelectMenu options={options} option={toOption} selectOption={setToOption} />
--- converter/src/components/Card/index.jsx
+++ converter/src/components/Card/index.jsx
@@ -10,6 +10,7 @@ export function Card() {
     const [toOption, setToOption] = useState('inr')
     const [convertedAmount, setConvertedAmount] = useState(0)
     const [data] = useCurrency(fromOption)
+    const [isDataLoaded, setIsDataLoaded] = useState(false)
 
     const setTextAmount = (value) => {
         const regex = /^[0-9]*(\.[0-9]*)?$/
@@ -19,18 +20,31 @@ export function Card() {
         }
     }
 
+    const switchOptions = () => {
+        setFromOption(toOption)
+        setToOption(fromOption)
+        setIsDataLoaded(false)
+    }
+
     useEffect(() => {
         if (Object.keys(data[fromOption] || {}).length > 0) {
             setConvertedAmount((Number(amount) * data[fromOption][toOption]).toFixed(2))
         }
     }, [amount])
 
+    useEffect(() => {
+        if (!isDataLoaded && Object.keys(data[fromOption] || {}).length > 0) {
+            setConvertedAmount((Number(amount) * data[fromOption][toOption]).toFixed(2))
+            setIsDataLoaded(true)
+        }
+    }, [fromOption, toOption, data])
+
     return (
         <div className="w-[88vw] sm:w-[85vw] md:w-[700px] h-[450px] md:h-[500px] mx-auto mt-[6vw] md:mt-12 px-[6%] md:px-12 py-[8%] md:py-16 bg-[#6B52FC] rounded-lg font-sans text-white flex flex-col justify-between">
             <h1 className=" text-xl sm:text-2xl md:text-3xl text-center font-semibold">Currency Converter</h1>
             <Amount amount={amount} setTextAmount={setTextAmount}/>
             {Object.keys(data[fromOption] || {}).length > 0 && (
-                <Converter options={Object.keys(data[fromOption])} fromOption={fromOption} toOption={toOption} setFromOption={setFromOption} setToOption={setToOption} />
+                <Converter options={Object.keys(data[fromOption])} fromOption={fromOption} toOption={toOption} setFromOption={setFromOption} setToOption={setToOption} switchOptions={switchOptions} />
             )}
             <Result amount={amount} convertedAmount={convertedAmount} fromOption={fromOption} toOption={toOption} />
         </div>
